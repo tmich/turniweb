c3266fad36407d4850039bd69d4ac24c83e5f969
tmich <tiziano_michelessi@yahoo.it>
2017-06-16 01:23:08 +0200

    Inserimento e modifica turni

--- turniweb.py
+++ turniweb.py
@@ -6,7 +6,7 @@ from flask_babel import Babel, format_datetime, format_date
 from flask_mail import Mail, Message
 from dateutil.parser import parse
 import jsonpickle
-from models import db, Dipendenti, Presenze, Assenze, Reparti
+from models import db, Dipendenti, Presenze, Assenze, Reparti, MotivoAssenza
 from anytree import Node, RenderTree
 
 app = Flask(__name__)
@@ -27,13 +27,13 @@ def crea_albero_presenze(dt1, dt2):
     
     giorni = []
     
-    presenze = (db.session.query(Presenze).
-        outerjoin(Dipendenti, Presenze.dipendente).
-        filter(Presenze.cancellato==0).
-        filter(Presenze.data_inizio.between(dt1, dt2)).
-        order_by(Dipendenti.cognome).
-        order_by(Presenze.data_inizio)
-    )
+    #presenze = (db.session.query(Presenze).
+        #outerjoin(Dipendenti, Presenze.dipendente).
+        #filter(Presenze.cancellato==0).
+        #filter(Presenze.data_inizio.between(dt1, dt2)).
+        #order_by(Dipendenti.cognome).
+        #order_by(Presenze.data_inizio)
+    #)
     
     dipendenti=db.session.query(Dipendenti).filter_by(cancellato=0).order_by('cognome')
     root=Node("root")
@@ -55,12 +55,21 @@ def crea_albero_presenze(dt1, dt2):
                 filter(Presenze.data_inizio.between(dt, dt + datetime.timedelta(days=1))).
                 order_by(Presenze.data_inizio)
             )
+
+            assenze = (db.session.query(Assenze).
+                filter(Assenze.cancellato==0).
+                filter(Assenze.dipendente_id==d.id).
+                filter(Assenze.data_inizio == dt).
+                order_by(Assenze.data_inizio)
+            )
+	    
             list_presenze=[ObjPresenza(p.id, p.data_inizio, p.data_fine, p.reparto) for p in presenze]
-            node_gg=Node("presenze", parent=node_dip, lines=[list_presenze])
+            list_assenze=[ObjAssenza(a.id, a.data_inizio, a.data_fine, a.motivo.descrizione, a.giornata_intera) for a in assenze]
+            node_gg=Node("giornata", parent=node_dip, presenze=[list_presenze], assenze=[list_assenze], data=dt, dip_id=d.id)
             dt = dt + datetime.timedelta(days=1)
     
-    for pre, fill, node in RenderTree(root):
-        print("%s%s" % (pre, node.name), file=sys.stderr)
+    #for pre, fill, node in RenderTree(root):
+    #    print("%s%s" % (pre, node.name), file=sys.stderr)
         
     return RenderTree(root), giorni, dipendenti
 
@@ -73,6 +82,17 @@ class ObjPresenza(object):
         
     def __repr__(self):
         return '%d %s %s %s' % (self.id, self.data_inizio, self.data_fine, self.reparto)
+      
+class ObjAssenza(object):
+    def __init__(self, id, data_inizio, data_fine, motivo, giornata_intera=False):
+        self.id = id
+        self.data_inizio = data_inizio
+        self.data_fine = data_fine
+        self.motivo = motivo
+        self.giornata_intera = giornata_intera
+        
+    def __repr__(self):
+        return '%d %s %s %s' % (self.id, self.data_inizio, self.data_fine, self.motivo)
 
 @app.route('/turni', methods=['GET', 'POST'])
 def turni():
@@ -86,11 +106,11 @@ def turni():
     
     tree, giorni, dipendenti = crea_albero_presenze(dt1, dt2)
     
-    #matrice = MatricePresenze(presenze, parse(dt1), parse(dt2))
     return render_template('turni.html', dipendenti=dipendenti, tree=tree, data_inizio=dt1, data_fine=dt2, giorni=giorni)
 
+@app.route('/nuovo_turno/<int:id_dipendente>/<string:data>', methods=['GET', 'POST'])
 @app.route('/nuovo_turno', methods=['GET', 'POST'])
-def nuovo_turno():
+def nuovo_turno(id_dipendente=0,data=date.today()):
     g.user={'name':'pippo'}
     dipendenti=db.session.query(Dipendenti).filter_by(cancellato=0).order_by('cognome')
     reparti=db.session.query(Reparti).filter_by(cancellato=0).order_by('nome')
@@ -112,7 +132,39 @@ def nuovo_turno():
         db.session.add(presenza)
         db.session.commit()
         flash('Turno inserito', category='success')
-    return render_template('nuovo_turno.html', dipendenti=dipendenti, reparti=reparti)
+        return redirect(url_for('turni'))
+    return render_template('nuovo_turno.html', dipendenti=dipendenti, reparti=reparti, id_dipendente=id_dipendente, data=data)
+
+@app.route('/modifica_turno/<int:id_presenza>', methods=['GET', 'POST'])
+def modifica_turno(id_presenza):
+    g.user={'name':'pippo'}
+    dipendenti=db.session.query(Dipendenti).filter_by(cancellato=0).order_by('cognome')
+    reparti=db.session.query(Reparti).filter_by(cancellato=0).order_by('nome')
+    presenza=db.session.query(Presenze).get(id_presenza)
+    
+    if request.method == 'POST':
+        f=request.form
+        id_pres=int(f['id_presenza'])
+        dip_id=int(f['dipendente'])
+        dtin=f['dtin']
+        tmin=f['tmin']
+        dtfi=f['dtfi']
+        tmfi=f['tmfi']
+        dt_inizio = time.strptime(dtin + " " + tmin, "%Y-%m-%d %H:%M")
+        dt_fine = time.strptime(dtfi + " " + tmfi, "%Y-%m-%d %H:%M")
+        reparto=f['reparto']
+        #return time.strftime("%d/%m/%Y %H:%M", dt_inizio)
+        data_inizio=datetime.datetime(dt_inizio.tm_year, dt_inizio.tm_mon, dt_inizio.tm_mday, dt_inizio.tm_hour, dt_inizio.tm_min, 0)
+        data_fine=datetime.datetime(dt_fine.tm_year, dt_fine.tm_mon, dt_fine.tm_mday, dt_fine.tm_hour, dt_fine.tm_min, 0)
+        
+        presenza.data_inizio=data_inizio
+        presenza.data_fine=data_fine
+        presenza.reparto=reparto
+        db.session.commit()
+        
+        flash('Turno aggiornato', category='success')
+        return redirect(url_for('turni'))
+    return render_template('modifica_turno.html', dipendenti=dipendenti, reparti=reparti, id_dipendente=presenza.dipendente_id, data_inizio=date.strftime(presenza.data_inizio, '%Y-%m-%d'), data_fine=date.strftime(presenza.data_fine, '%Y-%m-%d'), ora_inizio=date.strftime(presenza.data_inizio, '%H:%M'), ora_fine=date.strftime(presenza.data_fine, '%H:%M'), reparto=presenza.reparto, id_presenza=presenza.id)
 
 @app.route('/logout')
 def logout():
@@ -132,4 +184,5 @@ def _jinja2_filter_date(date, fmt=None):
         return format_date(date, 'medium')
 
 if __name__ == "__main__":
-    app.run(host='192.168.56.101', port=5000)
+    #app.run(host='192.168.56.101', port=5000)
+    app.run()
